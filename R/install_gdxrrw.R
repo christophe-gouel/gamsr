617caa8d9cabe786affd5e90ea44d5c05bd61461
Christophe Gouel <christophe.gouel@inra.fr>
2021-05-10 20:33:55 +0200

    Add installation function

---
+++ R/install_gdxrrw.R
@@ -0,0 +1,35 @@
+
+detect_last_version <- function() {
+  gdxrrw_url <- "https://support.gams.com/doku.php?id=gdxrrw:interfacing_gams_and_r"
+  gdxrrw_html <- rvest::read_html(gdxrrw_url)
+  alist <- rvest::html_elements(gdxrrw_html, "a")
+  last_version_url <- rvest::html_attr(alist[stringr::str_detect(alist,"tar.gz")][1],
+                                       "href")
+  last_version <- stringr::str_split(last_version_url, "gdxrrw_")[[1]][2]
+  last_version <- stringr::str_split(last_version, ".tar.gz")[[1]][1]
+  return(last_version)
+}
+
+select_ext <- function(OS) {
+  switch(OS,
+         "Windows" = "zip",
+         "Linux" = "tar.gz",
+         ... = "tgz")
+}
+
+install_gdxrrw <- function(version = "last", type = "platform_default") {
+
+  OS <- Sys.info()["sysname"]
+  if (type == "platform_default")
+    type <- ifelse(OS == "Windows", "binary", "source")
+  ext <- ifelse(type == "source", "tar.gz", select_ext(OS))
+  
+  if (version == "last") version <- detect_last_version()
+  
+  base_url <- "https://support.gams.com/_media/gdxrrw:gdxrrw_"
+  full_url <- paste0(base_url, version, ".", ext)
+  gdxrrw_file <- file.path(tempdir(), paste0("gdxrrw_", version, ".", ext))
+  download.file(full_url,
+                destfile = gdxrrw_file)
+  install.packages(gdxrrw_file, repos = NULL)
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gamsr
 Tools for tidy interactions between R and GAMS
+
+    devtools::install_github("christophe-gouel/gamsr");
